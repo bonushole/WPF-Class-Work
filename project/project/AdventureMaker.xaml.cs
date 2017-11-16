e8095fc4d1e95735776e4db7a816720fc487813c
Maximilian Evans <156max@gmail.com>
2017-11-15 19:21:46 -0500

    created class project!!!

---
+++ project/project/AdventureMaker.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for AdventureMaker.xaml
+    /// </summary>
+    public partial class AdventureMaker : Window
+    {
+        ObservableCollection<String> pointTypes = new ObservableCollection<string> {"passive", "bad ending"};
+        public AdventureMaker()
+        {
+            InitializeComponent();
+            pointTypeBox.ItemsSource = pointTypes;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string selected = pointTypeBox.SelectedItem.ToString();
+            switch (selected)
+            {
+                case ("passive"):
+                    {
+                        
+                    }
+                case ("bad ending"):
+                    {
+
+                    }
+                default:
+                    {
+                        this.Hide();
+                    }
+
+            }
+
+            Button button = new Button();
+            button.Content = "button";
+            pointsPanel.Children.Add(button);
+        }
+    }
+}
---
+++ project/project/GameFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    class GameFile
+    {
+        List<Plotpoint> plotpoints;
+        public GameFile()
+        {
+            plotpoints = new List<Plotpoint>();
+        }
+
+    }
+}
